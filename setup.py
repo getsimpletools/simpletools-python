d962763b5b23ac4e76e0b3906933750b11a32eef
Marcin <marcin@feedoptimise.com>
2018-01-29 19:23:21 +0000

    publish prepare

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='simpletools',
-      version='0.1',
+      version='0.1.1',
       description='simpletools',
-      url='https://github.com/getsimpletools/serviceq',
+      url='https://github.com/getsimpletools/simpletools-python/tree/master',
       author='Marcin Rosinski',
       author_email='marcin@getsimpletools.com',
-      license='BSD',
-      packages=['simpletools'],
+      license='BSD-3',
+      packages=['simpletools','serviceq'],
       zip_safe=False)
